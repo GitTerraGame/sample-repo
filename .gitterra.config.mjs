37aeeede711ef11385b655635638916bc255f322
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-13 16:43:39 -0400

    Update .gitterra.config.mjs

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,6 @@
 export default (config) => {
   config.timelapseLookBackPerfRun = 100;
-  config.createTimelapse = false;
+  config.createTimelapse = true;
   config.destinationURL = "https://gitterra.gitlab.io/testrepo/";
 
   return config;
