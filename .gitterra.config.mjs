badb7e03e87ddbb9f12dba1e64e841efc1a7b423
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-13 20:14:12 +0000

    Re-running the workflow

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,6 @@
 export default (config) => {
-  config.timelapseLookBackPerfRun = 100;
-  config.createTimelapse = true;
+  // config.timelapseLookBackPerfRun = 100;
+  // config.createTimelapse = false;
 
   return config;
 };
