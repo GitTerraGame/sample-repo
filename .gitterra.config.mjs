9cdee942da1da62471bf72c09f2e3538a4e4b7be
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-13 16:36:07 -0400

    Explicitly specifying the destination URL

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,7 @@
 export default (config) => {
   config.timelapseLookBackPerfRun = 100;
   config.createTimelapse = false;
+  config.destinationURL = "https://gitterra.gitlab.io/testrepo/";
 
   return config;
 };
