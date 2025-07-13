9c7a998bb7d63e81bb6b389e4c63d3290b5e3e63
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-13 20:17:24 +0000

    Bring timeline back

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,6 @@
 export default (config) => {
-  // config.timelapseLookBackPerfRun = 100;
-  // config.createTimelapse = false;
+  config.timelapseLookBackPerfRun = 100;
+  config.createTimelapse = false;
 
   return config;
 };
